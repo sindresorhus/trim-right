d68bf83eb6ef4ed906b552588f0c9ba03c1a93eb
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 19:09:39 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
-'use strict';
-var test = require('ava');
-var trimRight = require('./');
+import test from 'ava';
+import fn from './';
 
-test(function (t) {
-	t.assert(trimRight('  unicorn  ') === '  unicorn');
-	t.assert(trimRight('unicorn\r\n  \n') === 'unicorn');
-	t.assert(trimRight('unicorn\u00A0\uFEFF') === 'unicorn');
+test(t => {
+	t.is(fn('  unicorn  '), '  unicorn');
+	t.is(fn('unicorn\r\n  \n'), 'unicorn');
+	t.is(fn('unicorn\u00A0\uFEFF'), 'unicorn');
 
 	// zero-width space (zws), next line character (nel), non-character (bom) are not whitespace
-	t.assert(trimRight('\u200B\u0085\uFFFE') === '\u200B\u0085\uFFFE');
-
-	t.end();
+	t.is(fn('\u200B\u0085\uFFFE'), '\u200B\u0085\uFFFE');
 });
