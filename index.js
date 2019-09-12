5a8d094d470963f8112ee27d87f160a11fe45a58
Vano Devium <webdevium@gmail.com>
2019-09-12 21:48:08 +0300

    Removed useless RegExp loop operation for speed increasing

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 module.exports = string => {
 	let tail = string.length;
 
-	while (/[\s\uFEFF\u00A0]/.test(string[tail - 1])) {
-		tail--;
-	}
+	const matches = string.match(/[\s\uFEFF\u00A0]+$/);
+
+	tail -= (matches ? matches[0].length : 0);
 
 	return string.slice(0, tail);
 };
